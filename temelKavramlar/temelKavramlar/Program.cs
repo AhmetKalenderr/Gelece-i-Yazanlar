33e813d57ccc222b0cee2a0f7dc4a84df83c84c8
Ahmet <Ahmetkalender24@gmail.com>
2022-01-12 19:26:48 +0300

    first

---
+++ temelKavramlar/temelKavramlar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace temelKavramlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 257;
+            sbyte negatif = -128;
+
+
+            checked
+            {
+                byte deger = (byte)sayi;
+                Console.WriteLine(deger);
+            }
+        }
+    }
+}
